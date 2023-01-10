cd027b8389be959d1ad80a52c8dcfaeee874d697
Aayush <78782330+aauysh34@users.noreply.github.com>
2023-01-10 15:44:36 +0545

    first commit

---
+++ lib/app/user_permission.dart
@@ -0,0 +1,21 @@
+import 'package:permission_handler/permission_handler.dart';
+
+class UserPermission {
+  static Future<void> requestCameraPermission() async {
+    final cameraStatus = await Permission.camera.status;
+    if (cameraStatus.isDenied ||
+        cameraStatus.isRestricted ||
+        cameraStatus.isLimited) {
+      await Permission.camera.request();
+    }
+  }
+
+  static Future<void> requestLocationPermission() async {
+    final locationStatus = await Permission.location.status;
+    if (locationStatus.isDenied ||
+        locationStatus.isRestricted ||
+        locationStatus.isLimited) {
+      await Permission.location.request();
+    }
+  }
+}
